03129f489c1a1e71e1d65c9ddbc16c52850ff970
supriya <linkin.suppu@gmail.com>
2020-05-27 13:29:21 +0545

    Updates

---
+++ precache-manifest.e85f5744bc47bcf2861f22dc4c423335.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebaf1b5e066600c44953a09a06b3f8d",
+    "url": "/yts-movie-app/index.html"
+  },
+  {
+    "revision": "42dc378fbe5be3e0b968",
+    "url": "/yts-movie-app/static/css/main.735c3055.chunk.css"
+  },
+  {
+    "revision": "756230ee26adcc77a7a6",
+    "url": "/yts-movie-app/static/js/2.79dbb1f0.chunk.js"
+  },
+  {
+    "revision": "42dc378fbe5be3e0b968",
+    "url": "/yts-movie-app/static/js/main.fb15dac1.chunk.js"
+  },
+  {
+    "revision": "e2eefa1d2e20433693ee",
+    "url": "/yts-movie-app/static/js/runtime~main.1fcc84eb.js"
+  },
+  {
+    "revision": "202ea67c2f0b3061753e68b58a5dfe34",
+    "url": "/yts-movie-app/static/media/25.202ea67c.svg"
+  },
+  {
+    "revision": "eeda8ce46a584984dea25e1fefe5df56",
+    "url": "/yts-movie-app/static/media/background.eeda8ce4.jpg"
+  },
+  {
+    "revision": "a4282c8f3581c65de67fd990da7f2850",
+    "url": "/yts-movie-app/static/media/icon-search.a4282c8f.svg"
+  },
+  {
+    "revision": "c1cdb2b59d667cacfb2eaeb7f94de002",
+    "url": "/yts-movie-app/static/media/logo-YTS.c1cdb2b5.svg"
+  },
+  {
+    "revision": "78f40dc8373868bd6716760744c6e89e",
+    "url": "/yts-movie-app/static/media/star-icon green.78f40dc8.svg"
+  },
+  {
+    "revision": "676711979b1db833296473951a36d0ec",
+    "url": "/yts-movie-app/static/media/star-icon.67671197.svg"
+  }
+]);
